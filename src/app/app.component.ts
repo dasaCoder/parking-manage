678b0c80366a44cae210dbe8454cc1fc54d4aa26
dilushad <dilusha.dasanayaka@madmobile.com>
2021-02-09 02:58:26 +0530

    finalize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import { map } from 'rxjs/operators';
+import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,47 +12,146 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'parking-manage';
 
-  parking_slots = [];
+  parking_slots = {};
   selected_slot;
+  is_for_rent = false;
+  is_for_book = false;
+  vehicle_no:string;
+  date:string;
+  check_in:string;
+  reports_captions:string = "Reports";
+  displayedColumns = ["date"];
+  bookingDataSet: bookingData[] = [];
+  rentingDataSet: any[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
 
   ngOnInit() {
-    this.parking_slots = [
-      {
-        "name" : "Slot 1",
-        "division" : "D1",
-        "is_available" : "0",
-        "type":"car"
-      },
-      {
-        "name" : "Slot 2",
-        "division" : "D1",
-        "is_available" : "1",
-        "type":"car"
-      },
-      {
-        "name" : "Slot 3",
-        "division" : "D1",
-        "is_available" : "1",
-        "type":"bike"
-      },
-      {
-        "name" : "Slot 4",
-        "division" : "D1",
-        "is_available" : "0",
-        "type":"bike"
-      }
-    ];
+    // setInterval(this.set_parking_slots, 1000);
+    this.set_parking_slots();
+    this.load_bookings();
+    this.load_rentingss();
+  }
 
-    console.log(this.parking_slots[0]);
+  set_parking_slots() {
+    this.http.get("http://localhost:5000/blocks").subscribe(result => {
+      this.parking_slots = result;
+    });
+  }
+
+  select_slot_for_rent(slot) {
+    this.is_for_book = false;
+    this.is_for_rent = true;
+    this.select_slot(slot);
+  }
+
+  select_slot_for_book(slot) {
+    this.is_for_book = true;
+    this.is_for_rent = false;
+    this.select_slot(slot);
   }
 
   select_slot(slot) {
     this.selected_slot = slot;
   }
 
-  book_slot(slot) {
+  book_slot() {
+    let date = new Date(this.date);
+    let month = (date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1)) ;
+    let dateF = ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate()));
+    let formatted_date = date.getFullYear() + '/' + month + '/' + dateF;
+    let requestBody = {
+      "slot_id":this.selected_slot[0],
+      "vehicle_no": this.vehicle_no,
+      "date": formatted_date
+    }
+    console.log(requestBody);
+    this.http.post("http://localhost:5000/slot/book", requestBody).subscribe(data=>{
+      if(data['success']) {
+        this.selected_slot = undefined;
+        this.set_parking_slots();
+        alert("Booked");
+      } else {
+        alert("Error occured");
+      }
+    })
 
   }
 
+  rent_slot() {
+    let date = new Date();
+    let month = (date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1)) ;
+    let dateF = ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate()));
+    let formatted_date = date.getFullYear() + '/' + month + '/' + dateF;
+
+    let requestBody = {
+      "slot_id":this.selected_slot[0],
+      "vehicle_no": this.vehicle_no,
+      "date": formatted_date,
+      "check_in": this.check_in
+    }
+
+    this.http.post("http://localhost:5000/slot/rent", requestBody).subscribe(data=>{
+      if(data['success']) {
+        this.selected_slot = undefined;
+        this.set_parking_slots();
+        alert("Rented");
+      } else {
+        alert("Error occured");
+      }
+    })
+  }
+
+  end_rent_slot() {
+    let requestBody = {
+      "slot_id":this.selected_slot[0],
+      "check_out": this.check_in
+    }
+
+    this.http.post("http://localhost:5000/slot/rent/end", requestBody).subscribe(data=>{
+      if(data['success']) {
+        this.selected_slot = undefined;
+        this.set_parking_slots();
+        alert("Slot Released");
+      } else {
+        alert("Error occured");
+      }
+    })
+  }
+
+  toggle_reports() {
+    if(this.reports_captions == "Reports") {
+      this.reports_captions = "Home";
+    } else {
+      this.reports_captions = "Reports";
+    }
+  }
+
+  load_bookings() {
+    return this.http.get("http://localhost:5000/bookings")
+    .toPromise()
+    .then(res=> <any[]> res)
+    .then(data=>{
+      this.bookingDataSet = data;
+    })
+  }
+
+  load_rentingss() {
+    return this.http.get("http://localhost:5000/rentings")
+    .toPromise()
+    .then(res=> <any[]> res)
+    .then(data=>{
+      this.rentingDataSet = data;
+    })
+  }
 
 }
+
+export interface bookingData {
+  slot_id:string,
+  vehicle_no:string,
+  date:string
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
@@ -15,6 +18,8 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatRadioModule} from '@angular/material/radio';
 import { MatNativeDateModule } from '@angular/material/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
@@ -24,6 +29,8 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
     MatToolbarModule,
     MatCardModule,
     MatChipsModule,
@@ -33,9 +40,12 @@ import {MatIconModule} from '@angular/material/icon';
     MatDatepickerModule,
     MatNativeDateModule,
     MatRadioModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
